df5148f951237ab02854179ed82e80a284925256
Andreas Fredriksson <afredriksson89@gmail.com>
2024-05-08 14:25:44 +0200

    generate verification code done

---
+++ Services/VerificationService.cs
@@ -0,0 +1,149 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using VerificationProvider.Data.Contexts;
+using VerificationProvider.Functions;
+using VerificationProvider.Models;
+
+namespace VerificationProvider.Services;
+
+public class VerificationService(ILogger<VerificationService> logger, IServiceProvider serviceProvider) : IVerificationService
+{
+    private readonly ILogger<VerificationService> _logger = logger;
+    private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+    public string GenerateServiceBusEmailRequest(EmailRequest emailRequest)
+    {
+        try
+        {
+            var payload = JsonConvert.SerializeObject(emailRequest);
+            if (!string.IsNullOrEmpty(payload))
+            {
+                return payload;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.GenerateServiceBusEmailRequest() :: {ex.Message}");
+        }
+        return null!;
+    }
+
+    public async Task<bool> SaveVerificationRequest(VerificationRequest verificationRequest, string code)
+    {
+        try
+        {
+            using var context = _serviceProvider.GetRequiredService<DataContext>();
+
+            var existingRequest = await context.VerificationRequests.FirstOrDefaultAsync(x => x.Email == verificationRequest.Email);
+            if (existingRequest != null)
+            {
+                existingRequest.Code = code;
+                existingRequest.ExpiryDate = DateTime.Now.AddMinutes(5);
+                context.Entry(existingRequest).State = EntityState.Modified;
+            }
+            else
+            {
+                context.VerificationRequests.Add(new Data.Entities.VerificationRequestEntity()
+                {
+                    Email = verificationRequest.Email,
+                    Code = code,
+                    ExpiryDate = DateTime.Now.AddMinutes(5)
+                });
+            }
+
+            await context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.SaveVerificationRequest() :: {ex.Message}");
+        }
+        return false;
+    }
+
+    public VerificationRequest UnpackVerificationRequest(ServiceBusReceivedMessage message)
+    {
+        try
+        {
+            var verificationRequest = JsonConvert.DeserializeObject<VerificationRequest>(message.Body.ToString());
+            if (verificationRequest != null && !string.IsNullOrEmpty(verificationRequest.Email))
+            {
+                return verificationRequest;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.UnpackVerificationRequest() :: {ex.Message}");
+        }
+
+        return null!;
+    }
+
+    public string GenerateCode()
+    {
+        try
+        {
+            var rnd = new Random();
+            var code = rnd.Next(100000, 999999).ToString();
+
+            return code;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.GenerateCode() :: {ex.Message}");
+        }
+
+        return null!;
+    }
+
+    public EmailRequest GenerateEmailRequest(VerificationRequest verificationRequest, string code)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(verificationRequest.Email) && !string.IsNullOrEmpty(code))
+            {
+                var emailRequest = new EmailRequest
+                {
+                    To = verificationRequest.Email,
+                    Subject = $"Verification Code {code}",
+                    HtmlBody = $@"                    
+                        <html lang='en'>
+                        <head>
+                            <meta charset='UTF-8'>
+                            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                            <title>Verification Email</title>
+                        </head>
+                        <body style='font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0;'>
+                            <div style='background-color: #ffffff; width: 80%; max-width: 600px; margin: 20px auto; padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'>
+                                <div style='background-color:  #6366F1; color: #ffffff; padding: 10px; text-align: center;'>
+                                    <h1>Welcome to Silicon</h1>
+                                </div>
+                                <div style='padding: 20px; text-align: center;'>
+                                    <p>Thank you for signing up with {verificationRequest.Email}. Please enter the following verification code to activate your account:</p>
+                                    <p style='font-size: 18px; font-weight: bold;'>{code}</p>
+                                    <p>This code will expire in 30 minutes.</p>
+                                </div>
+                                <div style='font-size: 12px; text-align: center; color: #777777; padding: 10px;'>
+                                    <p>If you did not request this email, please ignore it.</p>
+                                </div>
+                            </div>
+                        </body>
+                        </html>                        
+                    ",
+                    PlainText = $"Please enter the following verification code to activate your account: {code}. If you did not request this email, please ignore it."
+                };
+
+                return emailRequest;
+            }
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCode.GenerateEmailRequest() :: {ex.Message}");
+        }
+        return null!;
+    }
+}
