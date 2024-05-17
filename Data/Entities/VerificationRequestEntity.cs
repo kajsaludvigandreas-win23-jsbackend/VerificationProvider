99a684b244bc44161e245bb0af0f82d88b86083a
Ludvig <ludvig.vinoy@hotmail.com>
2024-05-17 11:41:27 +0200

    new push

--- Data/Entities/VerificationRequestEntity.cs
+++ Data/Entities/VerificationRequestEntity.cs
@@ -7,5 +7,5 @@ public class VerificationRequestEntity
     [Key]
     public string Email { get; set; } = null!;
     public string Code { get; set; } = null!;
-    public DateTime ExpiryDate { get; set; } = DateTime.Now.AddMinutes(5);
+    public DateTime ExpiryDate { get; set; } = DateTime.Now.AddMinutes(30);
 }
